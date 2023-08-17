2cb3dab6a53958d0144422b98fa43194c63ad78a
hristachev <kot28052000@mail.ru>
2023-08-17 21:16:12 +0300

    Initial

---
+++ Source/AiTestWork/AiTestWorkGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AiTestWorkGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AITESTWORK_API AAiTestWorkGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
